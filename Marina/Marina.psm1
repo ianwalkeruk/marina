4ecd0c845e72b2977ed51496f3ff5ce621c38c68
Ian Walker <Ian.Walker@asos.com>
2018-04-06 08:50:30 +0100

    Added Gherkin steps

--- Marina/Marina.psm1
+++ Marina/Marina.psm1
@@ -1,2 +1,12 @@
 function Import-MarinaConfiguration {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,ParameterSetName='FromXmlFile')]
+		[string]$FileName
+		,
+		[Parameter(Mandatory=$false,ParameterSetName='FromXmlFile')]
+		[switch]$Xml
+	)
+	process {
+	}
 }
--- Marina/spec/Marina.steps.ps1
+++ Marina/spec/Marina.steps.ps1
@@ -1,8 +1,28 @@
+BeforeEachScenario {
+	$here = Split-Path -Parent $MyInvocation.MyCommand.ScriptBlock.File
+	$moduleParent = Resolve-Path (Join-Path $here '..\..')
+	Import-Module "$moduleParent\Marina"
+}
+
 Given 'we have a correctly-formatted configuration file' {
-	md testdrive:\config
-	Set-Content 'testdrive:\config\correct.xml' -Value @'
+	$script:ConfigFolder = 'testdrive:\config'
+	md $script:ConfigFolder
+	$script:ConfigFileName = Join-Path $script:ConfigFolder 'correct.xml'
+	Set-Content $script:ConfigFileName -Value @'
 <?xml version="1.0" encoding="utf-8"?>
 <marina />
 '@
-	'testdrive:\config\correct.xml' | Should Exist
+	$script:ConfigFileName | Should Exist
+}
+
+When 'we call Import-MarinaConfiguration' {
+	$script:config = Import-MarinaConfiguration -FileName $script:ConfigFileName
+}
+
+Then 'the configuration loads successfully' {
+	$script:config | Should -Not -BeNullOrEmpty
+}
+
+AfterEachScenario {
+	Remove-Module 'Marina'
 }
