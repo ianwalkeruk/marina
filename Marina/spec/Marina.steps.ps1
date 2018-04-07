31e703480b083e85d424c174c199b03ffb1c9edc
Ian Walker <ian.walker@walkeritc.co.uk>
2018-04-07 16:11:16 +0100

    Moved reference configuration to it's own file
    Broke setup of steps into logical BeforeEachFeature/Scenario blocks, better for pwsh
    
    Signed-off-by: Ian Walker <ian.walker@walkeritc.co.uk>

--- Marina/spec/Marina.steps.ps1
+++ Marina/spec/Marina.steps.ps1
@@ -1,24 +1,24 @@
-$ValidXml = @'
-<?xml version="1.0" encoding="utf-8"?>
-<marina />
-'@
+
+BeforeEachFeature {
+    $here = Split-Path -Parent $MyInvocation.MyCommand.ScriptBlock.File
+    $script:ValidXml = Get-Content (Join-Path $here 'reference_config.xml')
+    $script:moduleParent = Resolve-Path (Join-Path $here '..\..')
+    Import-Module "$moduleParent\Marina"
+}
 
 BeforeEachScenario {
-    $here = Split-Path -Parent $MyInvocation.MyCommand.ScriptBlock.File
-    $moduleParent = Resolve-Path (Join-Path $here '..\..')
     Import-Module "$moduleParent\Marina"
 }
 
 Given 'we have a correctly-formatted (?<type>\S+) configuration file' {
     param ($type)
-    $script:ConfigFolder = 'testdrive:\config'
-    md $script:ConfigFolder
+    $ConfigFolder = 'testdrive:\config'
+    md $ConfigFolder
     switch ($type.ToUpper()) {
         'XML' {
-            $script:ConfigFileName = Join-Path $script:ConfigFolder 'correct.xml'
-            Set-Content -Path $script:ConfigFileName -Value $ValidXml
-            Write-Host "|$(Get-Content -Path $script:ConfigFileName)|"
-            $script:ConfigFileName | Should Exist
+            $ConfigFileName = Join-Path $ConfigFolder 'correct.xml'
+            Set-Content -Path $ConfigFileName -Value $ValidXml
+            $ConfigFileName | Should Exist
         }
         default {
             throw "Configuration file type |$type| not known"
@@ -29,14 +29,14 @@ Given 'we have a correctly-formatted (?<type>\S+) configuration file' {
 When 'we call Import-MarinaConfiguration -FileName(?: -(?<type>.*))?' {
     param ($type)
     $params = @{
-        FileName = $script:ConfigFileName
+        FileName = $ConfigFileName
     }
     if ($type) {$params."$type" = $true}
-    $script:config = Import-MarinaConfiguration @params
+    $config = Import-MarinaConfiguration @params
 }
 
 Then 'the configuration loads successfully' {
-    $script:config | Should -Not -BeNullOrEmpty
+    $config | Should -Not -BeNullOrEmpty
 }
 
 AfterEachScenario {
